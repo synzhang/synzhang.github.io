c94e83664daeb51dfc56cd82ceb94fadf0eefb87
Syn Zhang <zeyanzhang.china@gmail.com>
2022-09-06 14:08:29 +0800

    feat: use google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,19 @@ class ExtendedDocument extends Document {
   render () {
     return (
       <Html lang='zh-CN'>
-        <Head />
+        <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+              `
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
